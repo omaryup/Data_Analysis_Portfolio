2109160c94e28bb8e218bd5527d35a933cace40a
Omar Yupanqui <yupanquit89@gmail.com>
2021-11-08 15:34:10 -0500

    Update Semiconductor_financials_compare.sql

--- Semiconductor_financials_compare.sql
+++ Semiconductor_financials_compare.sql
@@ -1,3 +1,19 @@
 SELECT *
 FROM INFORMATION_SCHEMA.TABLES
 WHERE table_type = 'BASE TABLE'
+
+CREATE TABLE Metrics
+(Ticker varchar(50),
+Company_name varchar(50),
+Year int,
+
+
+
+SELECT Items_in_Mil, [Oct. 25, 2020]
+FROM Semiconductors_financials.dbo.AMAT_IS
+
+SELECT Items_in_Mil, [Oct. 25, 2020]
+FROM Semiconductors_financials.dbo.AMAT_BS
+
+SELECT Items_in_Mil, [Oct. 25, 2020]
+FROM Semiconductors_financials.dbo.AMAT_CF
