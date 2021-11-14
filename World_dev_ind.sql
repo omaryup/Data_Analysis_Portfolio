66e6ded19fd4f7b08da22bf0c9123af8c3ac44d9
Omar Yupanqui <yupanquit89@gmail.com>
2021-11-14 08:39:24 -0500

    Update World_dev_ind.sql

--- World_dev_ind.sql
+++ World_dev_ind.sql
@@ -16,8 +16,10 @@ FROM World_Dev_ind.dbo.[2001_2005_world_dev_ind]
 --WHERE [Country Name] IS NULL
 ORDER BY [Country Name] ASC
 
-DELETE FROM World_Dev_ind.dbo.[2001_2005_world_dev_ind]
-WHERE [Country Name] IS NULL
+DELETE FROM 
+		World_Dev_ind.dbo.[2001_2005_world_dev_ind]
+WHERE 
+		[Country Name] IS NULL
 
 --Deleting Country Name fields that are NULL in 2006 - 2010 table
 SELECT *
@@ -25,8 +27,10 @@ FROM World_Dev_ind.dbo.[2006_2010_world_dev_ind]
 --WHERE [Country Name] IS NULL
 ORDER BY [Country Name] ASC
 
-DELETE FROM World_Dev_ind.dbo.[2006_2010_world_dev_ind]
-WHERE [Country Name] IS NULL
+DELETE FROM 
+		World_Dev_ind.dbo.[2006_2010_world_dev_ind]
+WHERE 
+		[Country Name] IS NULL
 
 --Deleting Country Name fields that are NULL in 2011 - 2015 table
 SELECT *
@@ -34,8 +38,10 @@ FROM World_Dev_ind.dbo.[2011_2015_world_dev_ind]
 --WHERE [Country Name] IS NULL
 ORDER BY [Country Name] ASC
 
-DELETE FROM World_Dev_ind.dbo.[2011_2015_world_dev_ind]
-WHERE [Country Name] IS NULL
+DELETE FROM 
+		World_Dev_ind.dbo.[2011_2015_world_dev_ind]
+WHERE 
+		[Country Name] IS NULL
 
 --Deleting Country Name fields that are NULL in 2016 - 2020 table
 SELECT *
@@ -43,24 +49,34 @@ FROM World_Dev_ind.dbo.[2016_2020_world_dev_ind]
 --WHERE [Country Name] IS NULL
 ORDER BY [Country Name] ASC
 
-DELETE FROM World_Dev_ind.dbo.[2016_2020_world_dev_ind]
-WHERE [Country Name] IS NULL
+DELETE FROM 
+		World_Dev_ind.dbo.[2016_2020_world_dev_ind]
+WHERE 
+		[Country Name] IS NULL
 
 ------------------------------------------------------------------------------------------------
 
 --Additional Cleaning of Null Values on Unique_ID field
 
-DELETE FROM World_Dev_ind.dbo.[2001_2005_world_dev_ind]
-WHERE Unique_ID IS NULL
+DELETE FROM 
+		World_Dev_ind.dbo.[2001_2005_world_dev_ind]
+WHERE 
+		Unique_ID IS NULL
 
-DELETE FROM World_Dev_ind.dbo.[2006_2010_world_dev_ind]
-WHERE Unique_ID IS NULL
+DELETE FROM 
+		World_Dev_ind.dbo.[2006_2010_world_dev_ind]
+WHERE 
+		Unique_ID IS NULL
 
-DELETE FROM World_Dev_ind.dbo.[2011_2015_world_dev_ind]
-WHERE Unique_ID IS NULL
+DELETE FROM 
+		World_Dev_ind.dbo.[2011_2015_world_dev_ind]
+WHERE 
+		Unique_ID IS NULL
 
-DELETE FROM World_Dev_ind.dbo.[2016_2020_world_dev_ind]
-WHERE Unique_ID IS NULL
+DELETE FROM 
+		World_Dev_ind.dbo.[2016_2020_world_dev_ind]
+WHERE 
+		Unique_ID IS NULL
 
 
 ------------------------------------------------------------------------------------------------
@@ -97,18 +113,20 @@ FROM World_Dev_ind.dbo.[2006_2010_world_dev_ind]
 /*
 CREATE TABLE world_dev_ind_20yrs(
 Unique_ID varchar(250) PRIMARY KEY NOT NULL,
-Country_name varchar(250),
-Country_code varchar(250),
-Series_Name varchar(250)
+Country_Name varchar(255),
+Country_Code varchar(255),
+Series_Name varchar(255)
 )*/
 
 SELECT *
 FROM World_Dev_ind.dbo.world_dev_ind_20yrs
 
-INSERT INTO World_Dev_ind.dbo.world_dev_ind_20yrs (Unique_ID)
-SELECT Unique_ID
-FROM World_Dev_ind.dbo.[2001_2005_world_dev_ind]
-
+INSERT INTO 
+		World_Dev_ind.dbo.world_dev_ind_20yrs (Unique_ID)
+SELECT 
+		Unique_ID
+FROM 
+		World_Dev_ind.dbo.[2001_2005_world_dev_ind]
 
 ------------------------------------------------------------------------------------------------
 
@@ -123,6 +141,55 @@ FROM World_Dev_ind.dbo.[2001_2005_world_dev_ind]
 ORDER BY Unique_ID ASC
 
 SELECT *
+FROM World_Dev_ind.dbo.[2006_2010_world_dev_ind]
+ORDER BY Unique_ID ASC
+
+SELECT *
+FROM World_Dev_ind.dbo.[2011_2015_world_dev_ind]
+ORDER BY Unique_ID ASC
+
+SELECT *
+FROM World_Dev_ind.dbo.[2016_2020_world_dev_ind]
+ORDER BY Unique_ID ASC
+
+SELECT *
 FROM World_Dev_ind.dbo.world_dev_ind_20yrs
 JOIN World_Dev_ind.dbo.[2001_2005_world_dev_ind]
 ON World_Dev_ind.dbo.world_dev_ind_20yrs.Unique_ID = World_Dev_ind.dbo.[2001_2005_world_dev_ind].Unique_ID
+
+
+--Visualizing what the table will look like
+
+SELECT	
+		World_Dev_ind.dbo.world_dev_ind_20yrs.Country_Name, 
+		World_Dev_ind.dbo.world_dev_ind_20yrs.Country_Code, 
+		World_Dev_ind.dbo.world_dev_ind_20yrs.Series_Name
+
+FROM	
+		World_Dev_ind.dbo.world_dev_ind_20yrs
+
+JOIN
+		World_Dev_ind.dbo.[2001_2005_world_dev_ind]
+ON
+		World_Dev_ind.dbo.world_dev_ind_20yrs.Unique_ID = World_Dev_ind.dbo.[2001_2005_world_dev_ind].Unique_ID
+
+--Updating Table with columns from the 2001-2005 based on a JOIN
+
+UPDATE 
+		World_Dev_ind.dbo.world_dev_ind_20yrs
+SET 
+		World_Dev_ind.dbo.world_dev_ind_20yrs.Country_Name = World_Dev_ind.dbo.[2001_2005_world_dev_ind].[Country Name],
+		World_Dev_ind.dbo.world_dev_ind_20yrs.Country_Code = World_Dev_ind.dbo.[2001_2005_world_dev_ind].[Country Code],
+		World_Dev_ind.dbo.world_dev_ind_20yrs.Series_Name = World_Dev_ind.dbo.[2001_2005_world_dev_ind].[Series Name]
+FROM 
+		World_Dev_ind.dbo.world_dev_ind_20yrs
+JOIN 
+		World_Dev_ind.dbo.[2001_2005_world_dev_ind]
+ON 
+		World_Dev_ind.dbo.world_dev_ind_20yrs.Unique_ID = World_Dev_ind.dbo.[2001_2005_world_dev_ind].Unique_ID
+
+--Confirming updates
+
+SELECT *
+FROM World_Dev_ind.dbo.world_dev_ind_20yrs
+ORDER BY Unique_ID ASC
